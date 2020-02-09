40f76c824a4c24cbdfe75ab1940071b7f997699e
ndurbin <nick.durbin@gmail.com>
2020-02-09 15:41:35 -0500

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import reducer from './store/reducers/index';
 import App from './App';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+document.getElementById("root"));
