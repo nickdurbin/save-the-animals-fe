ecd5cf6d88229c95a2fd39a4c5446c45ca77ce1c
ndurbin <nick.durbin@gmail.com>
2020-02-09 15:37:26 -0500

    Added some components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
+import HomePage from './components/Home/HomePage';
+import LoginForm from './components/Login/LoginForm';
+import ResetPassword from './components/Login/ResetPassword';
+import RegisterForm from './components/Login/RegisterForm';
+import Profile from './components/Profile/Profile';
+import CreateCampaign from './components/Campaigns/CreateCampaign';
+import Campaign from './components/Campaigns/Campaign';
+import CampaignList from './components/Campaigns/CampaignList'
+import decode from 'jwt-decode';
+import { connect } from 'react-redux';
+import { setUserId } from './store/actions/userActions';
 
-function App() {
+
+function App({ setUserId, user_id }) {
+
+  useEffect(() => {
+    if (user_id === '' && localStorage.getItem('token')) {
+      const token = localStorage.getItem('token')
+      const { subject }= decode(token)
+
+      setUserId(subject)
+    }
+  })
+
   return (
-    <div className="App">
+    <>
+    <Navigation />
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/login' component={LoginForm}/>
+      <Route path='/reset-password' component={ResetPassword} />
+      <Route path='/register' component={RegisterForm} />
+      <Route path='/profile' component={Profile}/>
 
-    </div>
+      <Route path='/createcampaign' component={CreateCampaign} />
+      <Route path='/campaigns/:id' component={Campaign}/>
+      <Route path='/campaigns' component={CampaignList}/>
+    </Switch>
+    </>
   );
 }
 
-export default App;
+const mapStateToProps = ({ userReducer }) => ({
+  user_id: userReducer.user_id
+})
+
+export default connect(mapStateToProps, { setUserId })(withRouter(App));
