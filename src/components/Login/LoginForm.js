e39e4e8db808382aebe8df51560fdbdf4c26d139
ndurbin <nick.durbin@gmail.com>
2020-02-09 15:42:45 -0500

    Initial login form

---
+++ src/components/Login/LoginForm.js
@@ -0,0 +1,191 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { TextField } from 'formik-material-ui';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockIcon from '@material-ui/icons/Lock';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { axiosWithoutAuth as axios } from '../../utils/axiosUtils'
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" to="/">
+        Save The Animals
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: `url()`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+function LoginForm(props) {
+  const [users, setUsers] = useState([])
+  const classes = useStyles();
+  const [credentials, setCredentials] = useState({});
+
+  console.log(props.status)
+
+  const forwardUser = () => {(props.history.push('/'))};
+
+  useEffect(() => {
+    if (props.status) {
+      setUsers([ ...users, props.status ])
+      forwardUser();
+    }
+  }, [props.status, users])
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Form>
+            <Field
+              type="email"
+              name="email"
+              value={credentials.email}
+              onChange={handleChange}
+              component={TextField}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              autoComplete="email"
+            />
+            <Field
+              name="password"
+              type="password"
+              value={credentials.password}
+              onChange={handleChange}
+              label="Password"
+              component={TextField}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button 
+              disabled={props.isSubmitting}
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Login
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link to='/reset-password' variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link to='/register' variant="body2">
+                  {"Don't have an account? Register"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </Form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default withFormik({
+  mapPropsToValues({ email, password }) {
+    
+    return {
+      email: email || "",
+      password: password || "",
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email("Username not valid")
+      .required("Username is required"),
+    password: Yup.string()
+      .min(8, "Password must be 8 characters or longer")
+      .required("Password is required"),
+  }),
+
+  handleSubmit(credentials, { resetForm, setSubmitting, setStatus }) {
+    axios()
+    .post('/auth/login', credentials)
+    .then(res => {
+      setStatus(res.data)
+      localStorage.setItem('token', JSON.stringify(res.data.token))
+      console.log(res.data, 'User has been logged in!');
+      resetForm();
+      setSubmitting(false);
+    })
+    .catch(err => {
+      console.log(err.res, 'Failed to login user to the database!');
+      setSubmitting(false);
+    });  
+  }
+
+})(LoginForm);
